17fe547be80917616d3f93ad611aa16d95265977
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2022-04-18 16:49:38 -0500

    Updated test.py

--- test.py
+++ test.py
@@ -14,6 +14,8 @@ import lineGeo.blockGeo as bg
 
 import math
 
+
+
 streets = xo.getConstructionStreets()
 streetLines = []
 
@@ -62,7 +64,7 @@ for multipoint in resPoints:
     resPointAngles.append(multipointAngles)
 
 
-    
+
 houses = []
 points = []
 # for multipoint in resPoints:
@@ -75,15 +77,15 @@ for i in range(0, len(resPoints)):
         x = resPoints[i][j].x
         y = resPoints[i][j].y
         points.append(Point(x,y))
-        house = House(0,y,x).poly        
+        house = House(random.randint(0,0),y,x).poly
         angle = (-1*(resPointAngles[i][j])+90)
         house = af.rotate(house, angle)
         if ((j != 0) and (j != len(resPoints[i]))):
             houses.append(house)
-        
-blockLine = sg.LineString([(0,0),(100,0),(100,100),(0,100)])
-block = bg.Block(blockLine)
-block.populateWithTownhouses(20, 15)
+
+#blockLine = sg.LineString([(0,0),(100,0),(100,100),(0,100)])
+#block = bg.Block(blockLine)
+#block.populateWithTownhouses(20, 15)
 
 a = ShapelyMap ()
 
@@ -98,102 +100,32 @@ for house in houses:
 for point in points:
     a.addPoint(point)
 
+
+blocks = xo.getConstructionBlocks ()
+blockRings = []
+
+for block in blocks:
+    # Turn nodes into linearRing
+    points = []
+        # Turn nodes into points
+    for node in block:
+        point = Point(float(node.lat), float(node.lon))
+        points.append(point)
+    blockRing = LineString(MultiPoint(points))
+    blockRings.append(blockRing)
+
+
+blockLots = []
+
+for block in blockRings:
+    g = bg.Block(block)
+    g.createCourtyard(random.randint(22,24))
+    g.subdivideBlockEvenly(random.randint(21,25))
+    for lot in g.lots:
+        # for lot in block.lots:
+        if lot.poly.geom_type == 'Polygon':
+            a.addPolygon(lot.poly, Tag('building', 'yes'))
+
 a.exportToXml('a.osm')
 
-# STREET_WIDTH = 8
-# BLOCK_WIDTH = 75
 
-# a = ShapelyMap ()
-
-# pointA =  (0,10)
-# pointB =  (10,10)
-# pointC =  (10,0)
-# pointD =  (0,0)
-
-# areaABCD = Polygon([pointA, pointB, pointC, pointD])
-
-# houses = []
-
-# for j in range(0,9):
-#     for i in range(0, 9):
-#         house = House(random.randrange(1,6),0,0).poly
-#         if (house != 1):
-#             # forward facing house
-#             angle = 0
-#             setback = -1*(house.exterior.bounds[1]-STREET_WIDTH)+j*BLOCK_WIDTH
-#             house = translate(house, i*15, setback)
-#             house = rotate(house, angle, Point(0,0))
-#             houses.append(house)
-#         house = House(random.randrange(1,6),0,0).poly
-#         if (house != 1):
-#             # backward facing house
-#             angle = 180
-#             setback = -1*(house.exterior.bounds[1]-STREET_WIDTH)+j*BLOCK_WIDTH
-#             house = rotate(house, angle, Point(0,0))
-#             house = translate(house, i*15, setback+30)
-#             houses.append(house)
-
-# for j in range(0,9):
-#     for i in range(0, 9):
-#         house = House(random.randrange(1,6),0,0).poly
-#         if (house != 1):
-#             # forward facing house
-#             angle = 0
-#             setback = -1*(house.exterior.bounds[1]-STREET_WIDTH)+j*BLOCK_WIDTH
-#             house = translate(house, i*15+150, setback)
-#             house = rotate(house, angle, Point(0,0))
-#             houses.append(house)
-#         house = House(random.randrange(1,6),0,0).poly
-#         if (house != 1):
-#             # backward facing house
-#             angle = 180
-#             setback = -1*(house.exterior.bounds[1]-STREET_WIDTH)+j*BLOCK_WIDTH
-#             house = rotate(house, angle, Point(0,0))
-#             house = translate(house, i*15+150, setback+30)
-#             houses.append(house)
-
-
-# for house in houses:
-#     a.addPolygon(house, xmlOsm.Tag('building','yes'))
-
-
-# x = np.linspace(0,10,10)
-# y = np.linspace(0,10,10)
-
-# xs, ys = np.meshgrid(x,y,sparse=True)
-# points = [(0,0)]
-
-
-# # for i in range (0,len(x)):
-# #     for j in range(0,len(y)):
-# #         nx = x[i] + 150*random.random()
-# #         ny = y[j] + 150*random.random()
-# #         points.append((nx,ny))
-
-# for i in range (0,2):
-#     for j in range (0,8):
-#         x = 10*i #+ random.randint(-1,1)
-#         y = 10*j #+ random.randint(-1,1)
-#         points.append((x,y))
-    
-
-# points = MultiPoint(points)
-
-# farms = Farmland(points)
-
-# # houserow = HouseRow(5)
-
-# # for house in houserow.houses:
-# #     a.addPolygon(house.poly, xmlOsm.Tag('building','yes'))
-
-# # for farm in farms.regions:
-# #     # farm = farm.buffer(3)
-# #     # farm = farm.buffer(-5)
-# #     farm.simplify(0.4, preserve_topology=False)
-# #     a.addPolygon(farm, xmlOsm.Tag('building','yes'))
-
-# # b = MultiPoint([(1,0),(3,4),(4,1)]).convex_hull
-# # a.addPolygon(b, xmlOsm.Tag('building','yes'))
-
-
-# a.exportToXml()
