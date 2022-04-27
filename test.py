be99e712d62f28cc1a5827f7106c7b2200ecc977
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2022-04-27 16:40:37 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,11 @@ from xmlOsm import xmlOsm as xo
 import lineGeo.lineGeo as lg
 import shapely.affinity as af
 import lineGeo.blockGeo as bg
-
+import shapely.ops as ops
 import math
 
 
+SETBACK = 10
 
 streets = xo.getConstructionStreets()
 streetLines = []
@@ -33,14 +34,20 @@ resLines = []
 
 # Offset all streets on both sides
 for street in streetLines:
-    resLines.append(street.parallel_offset(20, 'left'))
-    resLines.append(street.parallel_offset(20, 'right'))
+    resLinesL = (street.parallel_offset(SETBACK, 'left'))
+    resLinesR = (street.parallel_offset(SETBACK, 'right'))
+    # Flip the multipoint
+    #resLinesL = ops.transform(lg.reverse, resLinesL)
+
+    # Append multipoints into resLines
+    resLines.append(resLinesR)
+    resLines.append(resLinesL)
 
 resPoints = []
 
 # Interpolate all streets
 for resLine in resLines:
-    resPoints.append(lg.getRandomInterpolationPoints(resLine,0,7,12,))
+    resPoints.append(lg.getRandomInterpolationPoints(resLine,0,10,100))
 
 resPointAngles = []
 
@@ -58,6 +65,9 @@ for multipoint in resPoints:
             angle1 = math.atan2(dy1, dx1)
             angle2 = math.atan2(dy2, dx2)
             angle = ((angle1+angle2)/2)*180/math.pi
+            print(angle)
+            #angle = math.atan2(dy1, dx1)*180/math.pi
+            #print(angle)
         else:
             angle = 0
         multipointAngles.append(angle)
@@ -72,32 +82,47 @@ points = []
 #         house = House(0,point.y, point.x)
 #         houses.append(house)
 
+# Invert the streetlines
+for i in range (0, len(streetLines)):
+    streetLines[i] = ops.transform(lg.flip, streetLines[i])
+
 for i in range(0, len(resPoints)):
     for j in range (0, len(resPoints[i])):
         x = resPoints[i][j].x
         y = resPoints[i][j].y
         points.append(Point(x,y))
+        angle = (-1*(resPointAngles[i][j])+90)
         house = House(random.randint(0,0),y,x).poly
-        house = Home(random.uniform(10,15), random.uniform(10,20), sg.Point(y,x))
+        house = Home2(random.uniform(10,15), random.uniform(15,25), sg.Point(y,x), angle)
         if random.random() > 0.5:
             house.makeRearEnclave()
+        #house.rotate(angle)
+        #shouse.setback(2)
         house = house.poly
-        angle = (-1*(resPointAngles[i][j])+90)
-        house = af.rotate(house, angle)
-        if (j > 1):
-            print(house.intersects(houses[-1]))
+        for h in houses:
+            house = house.difference(h)
         if (j > 1) and (house.intersects(houses[-1])):
-            if random.random() > 0.5:
+            if random.random() > 0.7:
                 # Union
                 house = house.union(houses[-1])
                 houses.pop()
-                print('union')
+                #print('union')
             else:
                 # Difference
                 house = house.difference(houses[-1])
-                print('difference')
+                #print('difference')
         if ((j != 0) and (j != len(resPoints[i]))):
-            houses.append(house)
+            houseValid = True
+            # Check if the house intersects a street
+            for street in streetLines:
+                if street.intersects(house):
+                    # If it does, indicate
+                    houseValid = False
+            if houseValid == True:
+                houses.append(house)
+
+
+plt.show()
 
 #blockLine = sg.LineString([(0,0),(100,0),(100,100),(0,100)])
 #block = bg.Block(blockLine)
@@ -110,8 +135,9 @@ a = ShapelyMap ()
 
 for house in houses:
     if (house.geom_type == 'Polygon'):
-        a.addPolygon(house, Tag('building','yes'))
-s
+        if(len(house.exterior.coords)) != 0:
+            a.addPolygon(house, Tag('building','yes'))
+
 blocks = xo.getConstructionBlocks ()
 blockRings = []
 
@@ -130,13 +156,17 @@ blockLots = []
 
 for block in blockRings:
     g = bg.Block(block)
-    g.createCourtyard(random.randint(10,12))
-    g.subdivideBlockEvenly(random.randint(20,22))
+    g.createCourtyard(random.randint(22,28))
+    g.distortCourtyard(15)
+    courtyard = sg.Polygon(g.innerBoundary.coords)
+    a.addPolygon(courtyard, Tag('a', 'a'))
+    g.subdivideBlockEvenly(random.randint(50,55))
     for lot in g.lots:
         # for lot in block.lots:
         if lot.poly.geom_type == 'Polygon':
-            a.addPolygon(lot.poly, Tag('building', 'yes'))
+            tag = Tag('building', 'yes')
+            a.addPolygon(lot.poly, tag)
 
-a.exportToXml('a.osm')
+a.exportToXml('z.osm')
 
 
