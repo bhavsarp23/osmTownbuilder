1222c3856abca36bc2fb5820fe4d2757e3e9d2fd
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2022-04-27 16:41:22 -0500

    Update lineGeo.py

--- lineGeo/lineGeo.py
+++ lineGeo/lineGeo.py
@@ -1,10 +1,22 @@
 from shapely.geometry import *
 from shapely.geometry import LineString
 from shapely.ops import unary_union
+import shapely.ops as ops
 import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
+
+def getFlippedPoint (a):
+    return Point(a.y, a.x)
+
+def flip (x,y):
+    return y,x
+
+def reverse(x, y, z=None):
+    if z:
+        return x[::-1], y[::-1], z[::-1]
+    return x[::-1], y[::-1]
 
 # Returns a cartesian point based on a polar pair
 def polarToCart (rho, angle, degrees=True):
@@ -18,10 +30,13 @@ def getMidpointOfTwoPoints (point1, point2):
     return Point ((point1.x + point2.x)/2, (point1.y + point2.y)/2)
 
 # Return the angle of a line segment of two points
-def getAngleOfTwoPoints (point1, point2):
+def getAngleOfTwoPoints (point1, point2, radian=False):
     dy = point2.y - point1.y
     dx = point2.x - point1.x
-    return math.atan2 (dy,dx)
+    angle = math.atan2 (dy,dx)
+    if radian == False:
+        angle = angle*180/math.pi
+    return angle
 
 # Returns the slope from point 1 to point 2
 def getSlopeOfTwoPoints (point1, point2):
@@ -58,8 +73,7 @@ def getInterpolatedPointsByNumber (line, numberOfPoints):
 # Returns the third point of an isosceles triangle given a height and two points
 def getNormalOffsetPoint (point1, point2, offset):
     angle = getAngleOfTwoPoints (point1, point2)
-    normalAngle = angle - math.pi/2
-    normalAngle*180/math.pi
+    normalAngle = angle - 90
     point3Relative = polarToCart (offset, normalAngle)
     point3Base = getMidpointOfTwoPoints (point1, point2)
     point3 = Point (point3Base.x + point3Relative.x, point3Base.y + point3Relative.y)
