9c04a373d72230b46ea4a7b52110a216fa9b934f
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2022-08-12 07:39:01 -0500

    Update osmElements.py

--- osmElements.py
+++ osmElements.py
@@ -3,7 +3,7 @@
 # --------------------------------- #
 #             Libraries             #
 # --------------------------------- #
-import lxml.etree as et
+from lxml import etree as et
 import time
 
 # --------------------------------- #
@@ -20,6 +20,70 @@ def getRandomId():
 def getCoordinateFromMeter(value):
   return value/METER_CONV
 
+def getNodesByRef (osmRoot, refs):
+    nodes = []
+    for ref in refs:
+        for item in osmRoot.findall('node'):
+            if item.get('id') == ref:
+                lat = float(item.get('lat'))*METER_CONV*METER_CONV
+                lon = float(item.get('lon'))*METER_CONV*METER_CONV
+                nodes.append(Node(lat,lon))
+    return nodes
+
+def checkForConstructionHighway (way):
+    for element in way.findall('tag'):
+        if ((element.get('k') == 'highway') & (element.get('v') == 'construction')):
+            return True
+        else:
+            return False
+
+def checkForConstructionLanduse (way):
+    for element in way.findall('tag'):
+        if ((element.get('k') == 'landuse') & (element.get('v') == 'construction')):
+            return True
+        else:
+            return False
+
+
+def getConstructionStreets ():
+    osmFile = 'output.osm'
+    tree = et.parse(osmFile)
+    osmRoot = tree.getroot()
+
+    ways = []
+    # Find all ways
+    for item in osmRoot.findall('way'):
+        nodeIds = []
+        # Find ways with the tag highway=residential
+        if checkForConstructionHighway(item) == True:
+            # Add get all nodes in the way
+            for element in item.findall('nd'):
+                nodeIds.append(element.get('ref'))
+            # Make a lineString based on the nodes
+            wayNodes = getNodesByRef(osmRoot, nodeIds)
+            way = Way(wayNodes)
+            ways.append(way) 
+    return (ways)
+
+def getConstructionBlocks ():
+    osmFile = 'output.osm'
+
+    tree = et.parse(osmFile)
+    osmRoot = tree.getroot()
+    ways = []
+    # Find all ways
+    for item in osmRoot.findall('way'):
+        nodeIds = []
+        # Find ways with the tag highway=residential
+        if checkForConstructionLanduse(item) == True:
+            # Add get all nodes in the way
+            for element in item.findall('nd'):
+                nodeIds.append(element.get('ref'))
+            # Make a lineString based on the nodes
+            ways.append(getNodesByRef(osmRoot, nodeIds))
+    return (ways)
+
+
 # --------------------------------- #
 #              Classes              #
 # --------------------------------- #
@@ -29,7 +93,7 @@ class Tag:
     self.k = k
     self.v = v
 
-  def getOsmElement(self):
+  def getXmlElement(self):
     element = et.Element('tag')
     element.set('k', self.k)
     element.set('v', self.v)
@@ -52,7 +116,7 @@ class Node:
   def addRole(self, role):
     self.role = role
 
-  def getOsmElement(self):
+  def getXmlElement(self):
     element = et.Element('node')
     element.set('id', self.id)
     element.set('action', 'modify')
@@ -60,7 +124,7 @@ class Node:
     element.set('lat', self.lat)
     element.set('lon', self.lon)
     for tag in self.tags:
-      element.append(tag.getOsmElement())
+      element.append(tag.getXmlElement())
     return element
 
   def getRefElement(self):
@@ -79,10 +143,14 @@ class Node:
 
 class Way:
 
-  def __init__(self, listOfRefs):
+  def addNodes(self, nodes):
+    self.nodes = nodes
+
+  def __init__(self, listOfRefs, *argv, **kwargs):
     self.refs = listOfRefs
-    self.id = getRandomId()
-    self.tags = []
+    self.id = kwargs.get('id', getRandomId())
+    self.tags = kwargs.get('tags', [])
+    self.role = kwargs.get('role', "")
 
   def addTags(self,listOfTags=[]):
     self.tags.extend(listOfTags)
@@ -90,7 +158,7 @@ class Way:
   def addRole(self, role):
     self.role = role
 
-  def getOsmElement(self):
+  def getXmlElement(self):
     element = et.Element('way')
     element.set('id', self.id)
     element.set('action', 'modify')
@@ -100,7 +168,7 @@ class Way:
       element.append(ref.getRefElement())
 
     for tag in self.tags:
-      element.append(tag.getOsmElement())
+      element.append(tag.getXmlElement())
 
     return element
 
@@ -110,10 +178,10 @@ class Way:
     element.set('ref', self.id)
     if self.role != None:
       element.set('role',self.role)
-    return element
+    return element  
 
 class Area(Way):
-  def getOsmElement(self):
+  def getXmlElement(self):
     element = et.Element('way')
     element.set('id', self.id)
     element.set('action', 'modify')
@@ -126,7 +194,7 @@ class Area(Way):
     element.append(self.refs[0].getRefElement())
 
     for tag in self.tags:
-      element.append(tag.getOsmElement())
+      element.append(tag.getXmlElement())
 
     return element
 
@@ -140,7 +208,7 @@ class Relation:
   def addTags(self, listOfTags):
     self.tags.extend(listOfTags)
 
-  def getOsmElement(self):
+  def getXmlElement(self):
     element = et.Element('relation')
     element.set('id', self.id)
     element.set('action', 'modify')
@@ -149,11 +217,34 @@ class Relation:
       element.append(member.getMemberElement())
 
     for tag in self.tags:
-      element.append(tag.getOsmElement())
+      element.append(tag.getXmlElement())
 
     return element
 
 class OsmMap:
+
+  def __getWays(self, root):
+    ways = []
+    for wayElement in root.findall('way'):
+      id = wayElement.get('id')
+      tags = []
+      refs = []
+      for tagElement in wayElement.findall('tag'):
+        tags.append(Tag(tagElement.get('k'), tagElement.get('v')))
+      for refElement in wayElement.findall('nd'):
+        refs.append(refElement.get('ref'))
+      nodes = getNodesByRef(root, refs)
+      way = Way(nodes, nodes=nodes, tags=tags, id=id)
+      ways.append(way)
+    return ways
+
+  def __parseOsmFile(self, osmFile):
+
+    tree = et.parse(osmFile)
+
+    root = tree.getroot()
+    # Get ways
+    self.ways = self.__getWays(root)
 
   def __init__(self, string=''):
     if string != '':
@@ -162,20 +253,21 @@ class OsmMap:
     self.nodes = []
     self.ways = []
     self.relations = []
+    #self.__parseOsmFile('output.osm')
 
-  def getOsmElement(self):
+  def getXmlElement(self):
     element = et.Element('osm')
     element.set('version', '0.6')
     element.set('generator', 'script')
 
     for node in self.nodes:
-      element.append(node.getOsmElement())
+      element.append(node.getXmlElement())
 
     for way in self.ways:
-      element.append(way.getOsmElement())
+      element.append(way.getXmlElement())
 
     for relation in self.relations:
-      element.append(relation.getOsmElement())
+      element.append(relation.getXmlElement())
 
     return element
 
@@ -200,7 +292,7 @@ class OsmMap:
             self.addWays([member])
 
   def writeToFile(self, filename='output.osm'):
-    root = self.getOsmElement()
+    root = self.getXmlElement()
     tree = et.ElementTree(root)
     tree.write(filename,pretty_print=True)
 
@@ -221,5 +313,5 @@ if __name__=="__main__":
   relationABCDEF.addTags([Tag('type', 'multipolygon'), Tag('building', 'yes')])
   map.addWays([areaABC, areaDEF])
   map.addRelations([relationABCDEF])
-  map.writeToFile()
+  map.writeToFile("z.osm")
 
